d85d08a651db4ec6e01957ba30ce4c0675f325c7
Max Kremmel <xing@synapse.plus.com>
2006-11-06 15:41:06 +0000

    add some package information

---
+++ admin/schema_inc.php
@@ -0,0 +1,8 @@
+<?php
+global $gBitInstaller;
+$gBitInstaller->registerPackageInfo( GIGAUPLOAD_PKG_NAME, array(
+	'description' => "Gigaupload gives you a progressbar while uploading files. Please view gigaupload/INSTALL for installation instructions.",
+	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>',
+	'requires' => 'cgi-bin needs to be available.',
+) );
+?>
